23c7842bcc8c3d3e845340c1716dee190971e862
DubStray <francesco.gaming00@gmail.com>
2025-06-25 21:05:43 +0200

    First Commit

---
+++ src/main/java/com/francesco/app/simple_appointment_manager/SimpleAppointmnetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.francesco.app.simple_appointment_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppointmnetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppointmnetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francesco/app/simple_appointment_manager/SimpleAppointmnetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francesco.app.simple_appointment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppointmnetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
